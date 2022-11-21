af7b91e9a8424300c34164fc61ae0af5a79517e5
Türker Cakci <tuerker.cakici@adelt.it>
2022-11-21 13:14:11 +0100

    init

--- src/views/Apollo.vue
+++ src/views/Apollo.vue
@@ -12,13 +12,62 @@ export default {
       categories: []
     }
   },
-  apollo: {
-    categories: gql`{
-          categories{
-          id,
-          name
-          }
-      }`
+  methods:{
+    getUsers(){
+      const query = `
+                   query{
+                    users{
+                      id
+                      name
+                   }
+              }
+              `;
+
+      let variables = {
+        // page: page ? page : 1,
+        // perPage: 15,
+        // sortBy: this.sortBy
+      };
+
+      this.fetch(query, variables).then(res => {
+        console.log("res ", res)
+      })
+    },
+    fetch(query, variables = null){
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        })
+      }
+
+      return fetch('http://localhost:8888/graphiql', options).then(response => this.handleResponse(response))
+          .catch(err => this.handleError(err));
+    },
+    handleResponse(res){
+      return res.json().then(function (json) {
+        return res.ok ? json : Promise.reject(json);
+      });
+    },
+    handleError(err){
+      console.log("err: ", err)
+    },
+  // apollo: {
+  //   categories: gql`{
+  //         categories{
+  //         id,
+  //         name
+  //         }
+  //     }`
+  // }
+},
+mounted() {
+    this.getUsers()
   }
 }
 </script>
